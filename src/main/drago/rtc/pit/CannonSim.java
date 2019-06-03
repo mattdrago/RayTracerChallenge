d26db5c6b76155bfa565a58f990f2adfab66c0ed
Matt Drago <matt@drago.com.au>
2019-06-03 21:56:53 +1000

    Modifies Cannon Sim to plot the path of the cannon and to the save the plot to disk

--- src/main/drago/rtc/pit/CannonSim.java
+++ src/main/drago/rtc/pit/CannonSim.java
@@ -1,15 +1,29 @@
 package drago.rtc.pit;
 
+import drago.rtc.Canvas;
+import drago.rtc.Color;
+import drago.rtc.PPMWriter;
 import drago.rtc.Tuple;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class CannonSim {
     private Projectile projectile;
     private Environment environment;
+    private Canvas canvas;
 
-    private CannonSim(Projectile projectile, Environment environment) {
+    private static final Color PLOT_COLOR = new Color(0.9, 0.2, 0.1);
+
+    private CannonSim(Projectile projectile, Environment environment, Canvas canvas) {
 
         this.projectile = projectile;
         this.environment = environment;
+        this.canvas = canvas;
     }
 
     private void tick() {
@@ -23,15 +37,46 @@ public class CannonSim {
             tick();
             tickCount++;
             Tuple pos = projectile.position;
+            plot(pos);
             System.out.println(String.format("Current Position @ %d: %f, %f, %f", tickCount, pos.getX(), pos.getY(), pos.getZ()));
         }
     }
 
-    public static void main(String[] argv) {
-        Projectile projectile = new Projectile(Tuple.point(0, 1, 0), Tuple.vector(1, 1, 0).normalise());
-        Environment environment = new Environment(Tuple.vector(0, -0.1, 0), Tuple.vector(-0.01, 0, 0));
+    private void plot(Tuple pos) {
+        int x = (int) (pos.getX() + 0.5);
+        int y = (int) (pos.getY() + 0.5);
+        y = canvas.getHeight() - y;
+        canvas.writePixel(x, y, PLOT_COLOR);
+    }
 
-        CannonSim sim = new CannonSim(projectile, environment);
+    public static void main(String[] argv) {
+        Tuple start = Tuple.point(0, 1, 0);
+        Tuple velocity = Tuple.vector(1, 1.8, 0).normalise().scale(11.25);
+        Projectile projectile = new Projectile(start, velocity);
+
+        Tuple gravity = Tuple.vector(0, -0.1, 0);
+        Tuple wind = Tuple.vector(-0.01, 0, 0);
+        Environment environment = new Environment(gravity, wind);
+
+        Canvas canvas = new Canvas(900, 550);
+
+        CannonSim sim = new CannonSim(projectile, environment, canvas);
         sim.run();
+
+        sim.savePlot();
+    }
+
+    private void savePlot() {
+
+        Path outPath = Paths.get("plot.ppm");
+
+        try (BufferedWriter writer = Files.newBufferedWriter(outPath)) {
+            PPMWriter ppmWriter = new PPMWriter();
+            writer.write(ppmWriter.write(canvas));
+
+            System.out.println("Plot saved to: " + outPath);
+        } catch (IOException e) {
+            System.out.println("Unable to save plot: " + e.getMessage());
+        }
     }
 }
