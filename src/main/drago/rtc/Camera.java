8f837c71a87406c078e6013bec31ad384237d01d
Matt Drago <matt@drago.com.au>
2019-06-10 22:52:14 +1000

    Adds a Camera function.  The Camera can be "pointed" at the world and it will then render the world into an image.

---
+++ src/main/drago/rtc/Camera.java
@@ -0,0 +1,109 @@
+package drago.rtc;
+
+class Camera {
+    private int hSize;
+    private int vSize;
+    private double fieldOfView;
+    private Matrix transform;
+
+    private double halfWidth;
+    private double halfHeight;
+    private double pixelSize;
+
+    Camera(int hSize, int vSize, double fieldOfView) {
+        this.hSize = hSize;
+        this.vSize = vSize;
+        this.fieldOfView = fieldOfView;
+
+        this.transform = Matrix.identity(4);
+
+        calculatePixelSize();
+    }
+
+    int getHSize() {
+        return hSize;
+    }
+
+    void setHSize(int hSize) {
+        this.hSize = hSize;
+        calculatePixelSize();
+    }
+
+    int getVSize() {
+        return vSize;
+    }
+
+    void setVSize(int vSize) {
+        this.vSize = vSize;
+        calculatePixelSize();
+    }
+
+    double getFieldOfView() {
+        return fieldOfView;
+    }
+
+    void setFieldOfView(double fieldOfView) {
+        this.fieldOfView = fieldOfView;
+        calculatePixelSize();
+    }
+
+    Matrix getTransform() {
+        return transform;
+    }
+
+    void setTransform(Matrix transform) {
+        this.transform = transform;
+    }
+
+    double getPixelSize() {
+        return this.pixelSize;
+    }
+
+    private void calculatePixelSize() {
+        double halfView = Math.tan(fieldOfView / 2);
+        double aspect = (double)hSize / (double)vSize;
+
+        if (aspect >= 1) {
+            this.halfWidth = halfView;
+            this.halfHeight = halfView / aspect;
+        } else {
+            this.halfWidth = halfView * aspect;
+            this.halfHeight = halfView;
+        }
+
+        this.pixelSize = (this.halfWidth * 2) / hSize;
+    }
+
+    Ray rayForPixel(int px, int py) {
+
+        final double pixelCenter = 0.5;
+
+        double xOffset = ((double)px + pixelCenter) * this.pixelSize;
+        double yOffset = ((double)py + pixelCenter) * this.pixelSize;
+
+        double worldX = this.halfWidth - xOffset;
+        double worldY = this.halfHeight - yOffset;
+
+        Matrix invTransform = transform.inverse();
+
+        Tuple origin = invTransform.multiplyBy(Tuple.point(0, 0, 0));
+        Tuple pixel = invTransform.multiplyBy(Tuple.point(worldX, worldY, -1));
+        Tuple direction = pixel.subtract(origin).normalise();
+
+        return new Ray(origin, direction);
+    }
+
+    Canvas render(World world) {
+        Canvas image = new Canvas(hSize, vSize);
+
+        for (int y = 0; y < vSize; y++) {
+            for (int x = 0; x < hSize; x++) {
+                Ray ray = rayForPixel(x, y);
+                Color color = world.colorAt(ray);
+                image.writePixel(x, y, color);
+            }
+        }
+
+        return image;
+    }
+}
---
+++ src/test/drago/rtc/CameraTest.java
@@ -0,0 +1,89 @@
+package drago.rtc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CameraTest {
+
+    private static final double EPSILON = 0.000001;
+
+    @Test
+    void constructingACamera() {
+        int hSize = 160;
+        int vSize = 120;
+        double fieldOfView = Math.PI / 2;
+
+        Camera c = new Camera(hSize, vSize, fieldOfView);
+
+        assertEquals(hSize, c.getHSize());
+        assertEquals(vSize, c.getVSize());
+        assertEquals(fieldOfView, c.getFieldOfView());
+        assertEquals(Matrix.identity(4), c.getTransform());
+    }
+
+    @Test
+    void thePixelSizeForAHorizontalCanvas() {
+        Camera c = new Camera(200, 125, Math.PI / 2);
+
+        double expectedPixelSize = 0.01;
+
+        assertTrue(Math.abs(expectedPixelSize - c.getPixelSize()) < EPSILON);
+    }
+
+    @Test
+    void thePixelSizeForAVerticalCanvas() {
+        Camera c = new Camera(125, 200, Math.PI / 2);
+
+        double expectedPixelSize = 0.01;
+
+        assertTrue(Math.abs(expectedPixelSize - c.getPixelSize()) < EPSILON);
+    }
+
+    @Test
+    void constructingARayThroughTheCenterOfTheCanvas() {
+        Camera c = new Camera(201, 101, Math.PI / 2);
+        Ray r = c.rayForPixel(100, 50);
+
+        assertEquals(Tuple.point(0, 0, 0), r.getOrigin());
+        assertEquals(Tuple.vector(0, 0, -1), r.getDirection());
+    }
+
+    @Test
+    void constructingARayThroughACornerOfTheCanvas() {
+        Camera c = new Camera(201, 101, Math.PI / 2);
+        Ray r = c.rayForPixel(0, 0);
+
+        assertEquals(Tuple.point(0, 0, 0), r.getOrigin());
+        assertEquals(Tuple.vector(0.66519, 0.33259, -0.66851), r.getDirection());
+    }
+
+    @Test
+    void constructingARayWhenTheCameraIsTransformed() {
+        Camera c = new Camera(201, 101, Math.PI / 2);
+        c.setTransform(Matrix.rotationY(Math.PI / 4).multiplyBy(Matrix.translation(0, -2, 5)));
+
+        Ray r = c.rayForPixel(100, 50);
+
+        assertEquals(Tuple.point(0, 2, -5), r.getOrigin());
+        assertEquals(Tuple.vector(Math.sqrt(2) / 2, 0, -Math.sqrt(2) / 2), r.getDirection());
+    }
+
+    @Test
+    void renderingAWorldWithACamera() {
+        World w = World.defaultWorld();
+        Camera c = new Camera(11, 11, Math.PI / 2);
+
+        Tuple from = Tuple.point(0, 0, -5);
+        Tuple to = Tuple.point(0, 0, 0);
+        Tuple up = Tuple.vector(0, 1, 0);
+
+        Matrix viewTransform = Matrix.viewTransform(from, to, up);
+
+        c.setTransform(viewTransform);
+
+        Canvas image = c.render(w);
+
+        assertEquals(new Color(0.38066, 0.47583, 0.2855), image.pixelAt(5, 5));
+    }
+}
